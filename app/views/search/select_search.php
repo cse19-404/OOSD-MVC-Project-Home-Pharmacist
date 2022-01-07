03f19e9393651d754061512fe4d8dbe4ec0f6814
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-01-08 03:57:13 +0530

    search 3

--- app/views/search/select_search.php
+++ app/views/search/select_search.php
@@ -35,23 +35,91 @@
         margin-top: 1cm;
     }
 
+    ul{
+    margin:0;
+    padding:0;
+    list-style:none;
+    }
+    .padding-lg {
+        display: block;
+        padding-top: 60px;
+        padding-bottom: 60px;
+    }
+    .our-webcoderskull .cnt-block:hover {
+        box-shadow: 0px 0px 10px rgba(0,0,0,0.3);
+        border: 0;
+    }
 
+    .our-webcoderskull .cnt-block{ 
+    float:left; 
+    width:100%; 
+    background:#fff; 
+    padding:30px 20px; 
+    text-align:center; 
+    border:2px solid #d5d5d5;
+    margin: 0 0 28px;
+    }
+    .our-webcoderskull .cnt-block figure{
+    width:148px; 
+    height:148px; 
+    border-radius:100%; 
+    display:inline-block;
+    margin-bottom: 15px;
+    }
+    .our-webcoderskull .cnt-block img{ 
+    width:148px; 
+    height:148px; 
+    border-radius:100%; 
+    }
+    .our-webcoderskull .cnt-block h3{ 
+    color:#2a2a2a; 
+    font-size:20px; 
+    font-weight:500; 
+    padding:6px 0;
+    text-transform:uppercase;
+    }
+    .our-webcoderskull .cnt-block h3 a{
+    text-decoration:none;
+        color:#2a2a2a;
+    }
+    .our-webcoderskull .cnt-block h3 a:hover{
+        color:#337ab7;
+    }
+    .our-webcoderskull .cnt-block p{ 
+    color:#2a2a2a; 
+    font-size:13px; 
+    line-height:20px; 
+    font-weight:400;
+    }
 </style>
 <?php include_once('css/baseForm.php'); ?>
 
 <body>
     <div class='container-fluid'>
         <?php if (!isset($this->searchMode)) { ?>
-            <div>
-                <a href="<?= SROOT ?>CustomerDashboard/selectSearch/selected">
-                    <h2>Select a Pharmacy</h2>
-                </a>
-                <a href="<?= SROOT ?>PrefilledformHandler/nearBy">
-                    <h2>Search in Nearby Pharmacies</h2>
-                </a>
-                <a href="<?= SROOT ?>PrescriptionHandler/selectMethod">
-                    <h2>Upload a Prescription</h2>
-                </a>
+            <div class='container'>
+            <section class="our-webcoderskull padding-lg">
+                <ul class="row">
+                    <li class="col-12 col-md-6 col-lg-3">
+                        <div class="cnt-block equal-hight" style="height: 349px;" onclick="location.href='<?= SROOT ?>CustomerDashboard/selectSearch/selected';">
+                            <figure><img src="https://cdn2.iconfinder.com/data/icons/pharmacy-17/2000/Pharmacy_front-512.png" class="img-responsive" alt=""></figure>
+                            <h3><a href="<?= SROOT ?>CustomerDashboard/selectSearch/selected">Select a Pharmacy</h3>
+                        </div>
+                    </li>
+                    <li class="col-12 col-md-6 col-lg-3">
+                        <div class="cnt-block equal-hight" style="height: 349px;" onclick="location.href='<?= SROOT ?>PrefilledformHandler/nearBy';">
+                            <figure><img src="https://thumbs.dreamstime.com/b/pharmacy-location-blue-map-pin-icon-element-map-point-mobile-concept-web-apps-icon-website-design-109712535.jpg" class="img-responsive" alt=""></figure>
+                            <h3><a href="<?= SROOT ?>PrefilledformHandler/nearBy">Search in Nearby Pharmacies</h3>
+                        </div>
+                    </li>
+                    <li class="col-12 col-md-6 col-lg-3">
+                        <div class="cnt-block equal-hight" style="height: 349px;" onclick="location.href='<?= SROOT ?>PrescriptionHandler/selectMethod';">
+                            <figure><img src="https://media.istockphoto.com/vectors/medical-prescription-vector-illustration-rx-concepts-modern-flat-vector-id1136667779?k=20&m=1136667779&s=612x612&w=0&h=fUimtkr9BCbqkymYD6tXiQxh9RCspWLKwChy5JX3gvw=" class="img-responsive" alt=""></figure>
+                            <h3><a href="<?= SROOT ?>PrescriptionHandler/selectMethod">Upload a Prescription</h3>
+                        </div>
+                    </li>
+                </ul>
+            </section>
             </div>
             <?php }
         if (isset($this->searchMode)) {
