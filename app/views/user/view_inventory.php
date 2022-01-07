da18a5924476fb72273416dcbdd6fd11fcd36773
BanulaMahen <banulakumarage@gmail.com>
2022-01-08 01:34:29 +0530

    css table

--- app/views/user/view_inventory.php
+++ app/views/user/view_inventory.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory</title>
-    <?php include_once('css/base.php'); ?>
+    <?php include_once('css/baseTable.php'); ?>
 </head>
 
 <body>
