a76027b7702a9372ae893c401fbab76dec14b383
BanulaMahen <banulakumarage@gmail.com>
2022-01-08 04:21:07 +0530

    Update nearbypharmforms.php

--- app/views/search/nearbypharmforms.php
+++ app/views/search/nearbypharmforms.php
@@ -5,15 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NearBy Pharmacy Forms</title>
+    <?php include_once('css/baseTable.php'); ?>
 </head>
 <body>
-    <div>
+    <div class='container-fluid'>
+        <h1 class="header">Availability of NearBy Pharmacies</h1>
+        <hr>
         <?php if (isset($this->availabilty) && !empty($this->availabilty)) {?>
-        <table>
+        <table class='table'>
             <?php foreach($this->availabilty as $entry=>$value) { ?>
             <tr>
             <td><a href="<?=SROOT?>PrefilledformHandler/processItems/<?=$entry?>/-1/<?=$this->preId?>"><?=$this->pharm_map[$entry]?></a></td>
-            <td><?= $value[0].' out of '. count($this->items) . "Items available"?></td>
+            <td style="color:#346702"><?= $value[0].' out of '. count($this->items) . "Items available"?></td>
             <!-- <td><?= 'Total Price: '.$_SESSION['TotalPrice']?></td> -->
             </tr>
             <?php }?>
@@ -26,7 +29,7 @@
         }
         echo "All Pharmacies Found 0 out of ".$count;}?>
 
+    <br><br><a role="button" class="btn btn-primary" href="<?=SROOT?>CustomerDashboard/search">Go Back</a>
     </div>
-    <br><br><a href="<?=SROOT?>CustomerDashboard/search">Go Back</a>
 </body>
 </html>
