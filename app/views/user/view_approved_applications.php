6a5275f4e9087d5c079c5a7ea78995207b46482d
Dakshina Ranmal <76484102+dakshina99@users.noreply.github.com>
2022-01-08 01:31:19 +0530

    add base table and base form

--- app/views/user/view_approved_applications.php
+++ app/views/user/view_approved_applications.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Approved Pharmacy Applications</title>
-    <?php include_once('css/base.php'); ?>
+    <?php include_once('css/baseTable.php'); ?>
 </head>
 
 <body>
