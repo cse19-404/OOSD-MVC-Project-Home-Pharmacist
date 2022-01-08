21e7f1d2c3f07a6352aec53806980041f1bd8e9a
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-01-09 01:10:07 +0530

    seasonal offer

---
+++ css/baseOffer.php
@@ -0,0 +1,35 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+
+<style>
+    body{
+            background-image: url("https://image.jimcdn.com/app/cms/image/transf/dimension=2000x1500:format=jpg/path/se7babe6f99715253/backgroundarea/ic7f0c4cff1c46402/version/1499091867/image.jpg");
+            background-repeat: repeat;
+            background-size: cover;
+            height:100vh;
+        }
+
+    .header{
+        font-family: 'Slabo 27px', serif;
+    }
+    .container-fluid{
+        background-color: rgba(226, 226, 226, 0.687);
+    }
+    .mybtn{
+        float: right;
+        margin-top: 20px;
+        margin-right: 30px;
+    }
+
+</style>
