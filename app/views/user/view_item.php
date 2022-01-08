104e6e35546e913f3e06e0d7c9e6d612505abaf7
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-01-09 00:35:32 +0530

    view item

--- app/views/user/view_item.php
+++ app/views/user/view_item.php
@@ -10,41 +10,56 @@
         .bg-danger {
             color: #FF0000;
         }
+
+        .Appcontainer {
+            z-index: 2;
+            border-radius: 15px;
+            background-color: #e9e9e9ed;
+            height: 15%;
+            width: 40%;
+            margin: auto;
+            margin-top: 2cm;
+            padding: 30px;
+            padding-left: 30px;
+            padding-right: 30px;
+            box-shadow: 10px 10px 50px 0.1px rgba(0, 0, 0, 0.664);
+        }
     </style>
+    <?php include_once('css/baseForm.php'); ?>
 </head>
 
 <body>
-    <h2>Inventory Item Form</h2>
-    <div class='bg-danger'>
-                    <?php if(isset($this->displayErrors)) {
-                        echo $this->displayErrors;
-                    } ?>
-                </div>
-    <form method="post" action="<?=SROOT?>ItemHandler/save/<?=$this->mode?>/<?=($this->mode ==='edit')?$this->itemData['id']:''?>">
+    <div class='container-fluid'>
+    <br><br><a  class="mybtn btn btn-primary" role="button" href="<?= SROOT ?>ItemHandler/view">Go to inventory</a>
+        <h2 class="header">Inventory Item Form</h2>
+        <div class="Appcontainer">
+            <div class='bg-danger'>
+                <?php if (isset($this->displayErrors)) {
+                    echo $this->displayErrors;
+                } ?>
+            </div>
+            <form class="form-horizontal" method="post" action="<?= SROOT ?>ItemHandler/save/<?= $this->mode ?>/<?= ($this->mode === 'edit') ? $this->itemData['id'] : '' ?>">
 
-        Item Name: <input type="text" name="name" value="<?=$this->itemData['name']?>" required>
-        <span class="bg-danger">* </span>
-        <br><br>
-        Item Code: <input type="text" name="code" value="<?=$this->itemData['code']?>" required>
-        <span class="bg-danger">*</span>
-        <br><br>
-        Prescription Needed?: <input type="checkbox" name="prescription_needed" <?php if ($this->itemData['prescription_needed'] == 1) {
-                                                                                echo "checked";
-                                                                            } ?>>
-        <span class="bg-danger"></span>
-        <br><br>
-        Quantity Unit: <input type="text" name="quantity_unit" value="<?=$this->itemData['quantity_unit']?>" required>
-        <span class="bg-danger">*</span>
-        <br><br>
-        Quantity: <input type="text" name="quantity" value="<?=$this->itemData['quantity']?>" required>
-        <span class="bg-danger">*</span>
-        <br><br>
-        Price per Unit Quantity(Rs.): <input type="text" name="price_per_unit_quantity" value="<?=$this->itemData['price_per_unit_quantity']?>" required>
-        <span class="bg-danger">*</span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-    <br><br><a href="<?=SROOT?>ItemHandler/view">Go to inventory</a>
+                <label> Item Name:</label> <input class="form-control" type="text" name="name" value="<?= $this->itemData['name'] ?>" required>
+                <br>
+                <label>Item Code:</label> <input class="form-control" type="text" name="code" value="<?= $this->itemData['code'] ?>" required>
+                <br>
+                <label>Prescription Needed? :</label><input type="checkbox" name="prescription_needed" <?php if ($this->itemData['prescription_needed'] == 1) {
+                                                                                                            echo "checked";
+                                                                                                        } ?>>
+                <br><br>
+                <label>Quantity Unit:</label> <input class="form-control" type="text" name="quantity_unit" value="<?= $this->itemData['quantity_unit'] ?>" required>
+                <br>
+                <label>Quantity:</label> <input class="form-control" type="text" name="quantity" value="<?= $this->itemData['quantity'] ?>" required>
+                <br>
+                <label>Price per Unit Quantity(Rs.):</label> <input class="form-control" type="text" name="price_per_unit_quantity" value="<?= $this->itemData['price_per_unit_quantity'] ?>" required>
+                <br>
+                <input class="btn btn-success" type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+
+
 </body>
 
 </html>
